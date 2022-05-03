45aeb42a7fe9c8fb63bca908d79492d7c7ecec52
Neissandy <neisandy.silva@gmail.com>
2022-05-03 16:46:13 -0300

    Versão 5

---
+++ js/details.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Funcionou mais ou menos
+    //Quantidade Produto
+    const quantityElement = document.querySelector('.button-quantity');
+    const sub = document.querySelector('.sub');
+    const add = document.querySelector('.add');
+    const quantity = document.querySelector('.value-quantity');
+
+    add.addEventListener('click', () => {
+      const newQtd = parseInt(quantity.innerText) + 1;
+      quantity.innerHTML = newQtd;
+    });
+
+    sub.addEventListener('click', () => {
+      const newQtd = parseInt(quantity.innerText) - 1;
+      if (newQtd >= 0) quantity.innerHTML = newQtd;
+    });
+
+
+    //Aparecer e desaparecer carrinho
+    const buttonCart = document.querySelector('.buttonCart');
+
+    buttonCart.addEventListener('click', () => {
+      const cartDesktop = document.getElementById('cart-desktop');
+      
+      console.log(buttonCart);
+    });
+  });
+
